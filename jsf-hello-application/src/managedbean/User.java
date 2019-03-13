834f29aa3056035539dc27b3be57ff9b6851fc62
Jason Wee <peichieh@gmail.com>
2019-03-13 23:09:25 +0800

    another jsf tutorial

---
+++ jsf-hello-application/src/managedbean/User.java
@@ -0,0 +1,18 @@
+package managedbean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean(name="user")
+@RequestScoped
+public class User {
+	String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
