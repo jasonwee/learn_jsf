3d5ff7c90133ddfc0480a71995c52c4967caa0f0
Jason Wee <peichieh@gmail.com>
2019-03-31 05:46:03 +0800

    langauges support

---
+++ alert_threshold/src/main/java/co/weetech/jwms/LocaleBean.java
@@ -0,0 +1,32 @@
+package co.weetech.jwms;
+
+import java.util.Locale;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class LocaleBean {
+
+    private Locale locale;
+
+    public void init() {
+        locale = FacesContext.getCurrentInstance().getExternalContext().getRequestLocale();
+    }
+
+    public Locale getLocale() {
+        return locale;
+    }
+
+    public String getLanguage() {
+        return locale.getLanguage();
+    }
+
+    public void setLanguage(String language) {
+        locale = new Locale(language);
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+    }
+
+}
