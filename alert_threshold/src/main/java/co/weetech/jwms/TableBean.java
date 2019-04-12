50745fe8c0bd2a7a67a52f624f47b36da1fe583f
Jason Wee <peichieh@gmail.com>
2019-04-12 10:59:54 +0800

    sample of usig table

---
+++ alert_threshold/src/main/java/co/weetech/jwms/TableBean.java
@@ -0,0 +1,81 @@
+package co.weetech.jwms;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+ 
+@ManagedBean
+@SessionScoped
+public class TableBean implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	/*
+	 * https://javaserverfaces.github.io/docs/2.3/managed-bean-javadocs/deprecated-list.html
+	 * https://javaee.github.io/tutorial/jsf-configure002.html
+	 * https://thomasleduc.wordpress.com/2014/06/01/jsf-managedbean-to-named/
+	 * https://stackoverflow.com/questions/38040947/the-type-managedbean-is-deprecated
+	 * 
+	 */
+
+	private static final Order[] orderList = new Order[] {
+		
+		new Order("A0001", "Intel CPU", 
+				new BigDecimal("700.00"), 1),
+		new Order("A0002", "Harddisk 10TB", 
+				new BigDecimal("500.00"), 2),
+		new Order("A0003", "Dell Laptop", 
+				new BigDecimal("11600.00"), 8),
+		new Order("A0004", "Samsung LCD", 
+				new BigDecimal("5200.00"), 3),
+		new Order("A0005", "A4Tech Mouse", 
+				new BigDecimal("100.00"), 10)
+	};
+	 
+	public Order[] getOrderList() {
+ 
+		return orderList;
+ 
+	}
+	
+	public static class Order{
+		
+		String orderNo;
+		String productName;
+		BigDecimal price;
+		int qty;
+
+		public Order(String orderNo, String productName, BigDecimal price, int qty) {
+			this.orderNo = orderNo;
+			this.productName = productName;
+			this.price = price;
+			this.qty = qty;
+		}
+		
+		public String getOrderNo() {
+			return orderNo;
+		}
+		public void setOrderNo(String orderNo) {
+			this.orderNo = orderNo;
+		}
+		public String getProductName() {
+			return productName;
+		}
+		public void setProductName(String productName) {
+			this.productName = productName;
+		}
+		public BigDecimal getPrice() {
+			return price;
+		}
+		public void setPrice(BigDecimal price) {
+			this.price = price;
+		}
+		public int getQty() {
+			return qty;
+		}
+		public void setQty(int qty) {
+			this.qty = qty;
+		}
+	}
+}
